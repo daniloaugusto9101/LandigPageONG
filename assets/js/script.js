476850127ecff40e019d5b79c80ebce0c0173bf3
Danilo Augusto <danilloagt@gmail.com>
2022-12-18 17:18:12 -0300

    add Efect scrollreveal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+
+// Efeito Menu Stivk
+window.addEventListener("scroll", ()=>{
+    let header = document.querySelector("header");
+    header.classList.toggle("header__sticky", window.scrollY > 0);
+});
+
+// Exibe scrooll suave
+const menu = document.querySelectorAll('.menu a[href^="#"]')
+
+menu.forEach(item =>{
+    item.addEventListener('click', toScroll)
+})
+
+function toScroll(event){
+    event.preventDefault();
+    const element = event.target 
+    const id = element.getAttribute('href');
+
+    const section = document.querySelector(id)
+    const to = section.offsetTop;
+
+    window.scroll({
+        top: to - 80,
+        behavior: "smooth"
+    })
+}
+
+
+// Abre e fecha menu mobolie
+const navbar = document.querySelector('.nav-bar__container')
+btn.addEventListener('click', function(){
+    navbar.classList.toggle('active')
+})
+
+
+// Efeito abrir e fechar card de perguntas
 const cards = document.querySelectorAll(".cardP")
 
 cards.forEach(function(card) {
@@ -16,7 +53,8 @@ cards.forEach(function(card) {
 });
 
 
-window.addEventListener("scroll", ()=>{
-    let header = document.querySelector("header");
-    header.classList.toggle("header__sticky", window.scrollY > 0);
-});
+
+
+
+
+
