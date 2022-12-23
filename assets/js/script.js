8cacb72b8f4044ac14c2f978e08c0921a0e66ce8
Danilo Augusto <danilloagt@gmail.com>
2022-12-23 12:22:50 -0300

    Update slide card

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,39 @@ cards.forEach(function(card) {
     })
 });
 
+
+// Slide de iamgnes
+var swiper = new Swiper(".adoption__content", {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    // slidesPerGroup: 3,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3
+      }
+    }
+  });
+
 // Efeito Scrol revel
 // Window.sr = ScrollReveal({
 //     reset: true,
